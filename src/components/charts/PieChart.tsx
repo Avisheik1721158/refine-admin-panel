001624b8c49724e5c709b25bfa09427846d50a76
Avisheik1721158 <avisheikbarua960@gmail.com>
2023-06-25 16:55:33 +0600

    Dashboard Created

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Card, Col, Row, Typography } from "antd";
 
-export default function PieChart() {
+import { PieChartProps } from "interfaces/home";
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+export default function PieChart({
+  title,
+  value,
+  // series,
+  colors,
+}: PieChartProps) {
   return (
-    <div>PieChart</div>
-  )
+    <div>
+      <Typography style={{ fontSize: "14", color: "#808191" }}>
+        {title}
+      </Typography>
+      <Typography
+        style={{ fontSize: "24", fontWeight: "700", color: "#11142d" }}
+      >
+        {value}
+      </Typography>
+      {/* <ReactApexChart
+        options={{
+          chart: { type: "donut" },
+          colors,
+          legend: { show: false },
+          dataLabels: { enabled: false },
+        }}
+      /> */}
+    </div>
+  );
 }
-
--- src/pages/Dashboard/home.tsx
+++ src/pages/Dashboard/home.tsx
@@ -1,12 +1,84 @@
-
-import { useList } from "@refinedev/core"
-import { Typography } from 'antd';
+import { useList } from "@refinedev/core";
+import { Card, Col, Divider, Row, Typography } from "antd";
+import { PieChart } from "components";
 export default function Home() {
   return (
-     <Typography
-     
-     >
-     Dashboard
-     </Typography>
-  )
+    <Card>
+      <Typography style={{ fontSize: "25", fontWeight: "700" }}>
+        Dashboard
+      </Typography>
+      <Card>
+    
+    <Row gutter={16}>
+      <Col className="gutter-row" span={6}>
+        <div>
+        <PieChart
+              title="Properties for sale"
+              value={684}
+              series={[75, 25]}
+              colors={["#475be8", "#e4e8ef"]}
+            />
+        </div>
+      </Col>
+      <Col className="gutter-row" span={6}>
+        <div> <PieChart
+              title="Properties for Rent"
+              value={550}
+              series={[60, 40]}
+              colors={["#475be8", "#e4b8ef"]}
+            /></div>
+      </Col>
+      <Col className="gutter-row" span={6}>
+        <div>  <PieChart
+              title="Total Customers"
+              value={5684}
+              series={[75, 25]}
+              colors={["#275be8", "#c4e8ef"]}
+            /></div>
+      </Col>
+      <Col className="gutter-row" span={6}>
+        <div>
+        <PieChart
+              title="Properties for Cities"
+              value={555}
+              series={[75, 25]}
+              colors={["#475be8", "#c4e8ef"]}
+            />
+        </div>
+      </Col>
+    </Row>
+        {/* <Row gutter={} >
+           <div >
+            
+           <PieChart
+              title="Properties for sale"
+              value={684}
+              series={[75, 25]}
+              colors={["#475be8", "#e4e8ef"]}
+            />
+
+            <PieChart
+              title="Properties for Rent"
+              value={550}
+              series={[60, 40]}
+              colors={["#475be8", "#e4b8ef"]}
+            />
+          
+            <PieChart
+              title="Total Customers"
+              value={5684}
+              series={[75, 25]}
+              colors={["#275be8", "#c4e8ef"]}
+            />
+            <PieChart
+              title="Properties for Cities"
+              value={555}
+              series={[75, 25]}
+              colors={["#475be8", "#c4e8ef"]}
+            />
+           </div>
+        </Row> */}
+      </Card>
+    </Card>
+  );
 }
